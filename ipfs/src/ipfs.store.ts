87cb54ac27fcf7bc30fd5ed362d70aaf472b7dcf
Pepo <pepo@uprtcl.io>
2020-11-09 17:46:30 +0100

    add ipfs timeout

--- ipfs/src/ipfs.store.ts
+++ ipfs/src/ipfs.store.ts
@@ -12,7 +12,6 @@ import { Logger } from '@uprtcl/micro-orchestrator';
 
 import { IpfsConnectionOptions } from './types';
 import { sortObject } from './utils';
-import CID from 'cids';
 import { PinnedCacheDB } from './pinner.cache';
 
 export interface PutConfig {
@@ -22,9 +21,21 @@ export interface PutConfig {
   pin?: boolean;
 }
 
-const WAIT = 1000;
-const ATTEMPTS = 0;
-const ENABLE_LOG = false;
+const TIMEOUT = 10000;
+const ENABLE_LOG = true;
+
+const promiseWithTimeout = (
+  promise: Promise<any>,
+  timeout: number
+): Promise<any> => {
+  let timeoutId;
+  const timeoutPromise = new Promise((_, reject) => {
+    timeoutId = setTimeout(() => {
+      reject(new Error('Request timed out'));
+    }, timeout);
+  });
+  return Promise.race([promise, timeoutPromise]);
+};
 
 export class IpfsStore extends Connection implements CASStore {
   logger = new Logger('IpfsStore');
@@ -51,86 +62,6 @@ export class IpfsStore extends Connection implements CASStore {
     }
   }
 
-  public tryPut(
-    buffer: any,
-    putConfig: object,
-    wait: number,
-    attempt: number
-  ): Promise<any> {
-    return new Promise((resolve, reject) => {
-      if (ENABLE_LOG) {
-        this.logger.log(`Try put. Remaining attempts: ${attempt}`, {
-          client: this.client,
-          buffer,
-          putConfig,
-        });
-      }
-
-      let timeout;
-      if (attempt > 0) {
-        /** retry recursively with twice as much the wait time setting */
-        timeout = setTimeout(() => {
-          this.tryPut(buffer, putConfig, wait * 2, attempt - 1)
-            .then((result: any) => resolve(result))
-            .catch((e) => reject(e));
-        }, wait);
-      }
-
-      this.client.dag
-        .put(Buffer.from(buffer), putConfig)
-        .then((result: object) => {
-          clearTimeout(timeout);
-          resolve(result);
-        })
-        .catch((e) => {
-          this.logger.error(
-            `Error putting object on IPFS on attempt: ${attempt}`,
-            {
-              e,
-              client: this.client,
-              buffer,
-              putConfig,
-            }
-          );
-        });
-    });
-  }
-
-  public tryGet(hash: string, wait: number, attempt: number): Promise<any> {
-    let timeout;
-
-    if (CID.isCID(hash)) {
-      throw new Error(`Invalid CID ${hash}`);
-    }
-
-    return new Promise((resolve, reject) => {
-      if (ENABLE_LOG) {
-        this.logger.log(
-          `Trying to get ${hash}. Remaining attempts: ${attempt}`
-        );
-      }
-
-      let found = false;
-
-      /** retry recursively with twice as much the wait time setting */
-      if (attempt > 0) {
-        timeout = setTimeout(() => {
-          this.tryGet(hash, wait * 2, attempt - 1)
-            .then((result) => resolve(result))
-            .catch((e) => {
-              if (!found) reject(e);
-            });
-        }, wait);
-      }
-
-      this.client.dag.get(hash).then((result: any) => {
-        found = true;
-        clearTimeout(timeout);
-        resolve(result);
-      });
-    });
-  }
-
   /**
    * Adds a raw js object to IPFS with the given cid configuration
    */
@@ -153,37 +84,32 @@ export class IpfsStore extends Connection implements CASStore {
     };
 
     /** recursively try */
-    return this.tryPut(buffer, putConfig, WAIT, ATTEMPTS)
-      .then((result: any) => {
-        let hashString = result.toString(this.cidConfig.base);
-        if (ENABLE_LOG) {
-          this.logger.log(`Object stored`, {
-            object,
-            sorted,
-            buffer,
-            hashString,
-          });
-        }
-        if (this.pinnerUrl) {
-          this.pinnedCache.pinned.get(hashString).then((pinned) => {
-            if (!pinned) {
-              if (ENABLE_LOG) {
-                this.logger.log(`pinning`, hashString);
-              }
-              fetch(`${this.pinnerUrl}/pin_hash?cid=${hashString}`).then(
-                (response) => {
-                  this.pinnedCache.pinned.put({ id: hashString });
-                }
-              );
-            }
-          });
-        }
-        return hashString;
-      })
-      .catch((e) => {
-        this.logger.error('error', e);
-        throw new Error('Sorry but it seems impossible to store this on IPFS');
+    const result = await this.client.dag.put(Buffer.from(buffer), putConfig);
+    let hashString = result.toString(this.cidConfig.base);
+
+    if (ENABLE_LOG) {
+      this.logger.log(`Object stored`, {
+        object,
+        sorted,
+        buffer,
+        hashString,
       });
+    }
+    if (this.pinnerUrl) {
+      this.pinnedCache.pinned.get(hashString).then((pinned) => {
+        if (!pinned) {
+          if (ENABLE_LOG) {
+            this.logger.log(`pinning`, hashString);
+          }
+          fetch(`${this.pinnerUrl}/pin_hash?cid=${hashString}`).then(
+            (response) => {
+              this.pinnedCache.pinned.put({ id: hashString });
+            }
+          );
+        }
+      });
+    }
+    return hashString;
   }
 
   /**
@@ -192,21 +118,14 @@ export class IpfsStore extends Connection implements CASStore {
   async get(hash: string): Promise<object | undefined> {
     /** recursively try */
     if (!hash) throw new Error('hash undefined or empty');
-    return this.tryGet(hash, WAIT, ATTEMPTS)
-      .then((raw) => {
-        const forceBuffer = Uint8Array.from(raw.value);
-        let object = CBOR.decode(forceBuffer.buffer);
-        if (ENABLE_LOG) {
-          this.logger.log(`Object retrieved ${hash}`, { raw, object });
-        }
-        return object;
-      })
-      .catch((e) => {
-        this.logger.warn(
-          `Object with ${hash} not found in IPFS, returning undefined`,
-          e
-        );
-        return undefined;
-      });
+
+    const raw = await promiseWithTimeout(this.client.dag.get(hash), 10000);
+
+    const forceBuffer = Uint8Array.from(raw.value);
+    let object = CBOR.decode(forceBuffer.buffer);
+    if (ENABLE_LOG) {
+      this.logger.log(`Object retrieved ${hash}`, { raw, object });
+    }
+    return object;
   }
 }
